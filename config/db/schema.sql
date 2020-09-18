cba900da1b68f6baf1c93a0991d26829e12a6068
Matt Hunt <wmatthunt@gmail.com>
2020-09-18 16:06:21 -0400

    changed cats to burgers

--- config/db/schema.sql
+++ config/db/schema.sql
@@ -1,6 +1,6 @@
 ### Schema
-CREATE DATABASE pets_db;
-USE pets_db;
+CREATE DATABASE foods_db;
+USE foods_db;
 
 CREATE TABLE buyers
 (
@@ -9,13 +9,13 @@ CREATE TABLE buyers
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE pets
+CREATE TABLE foods
 (
-	id int NOT NULL AUTO_INCREMENT,
-	animal_breed varchar(255) NOT NULL,
-	animal_name varchar(255) NOT NULL,
-	price int NOT NULL,
-	buyer_id int NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (buyer_id) REFERENCES buyers(id)
+	-- id int NOT NULL AUTO_INCREMENT,
+	-- animal_breed varchar(255) NOT NULL,
+	-- animal_name varchar(255) NOT NULL,
+	-- price int NOT NULL,
+	-- buyer_id int NOT NULL,
+	-- PRIMARY KEY (id),
+	-- FOREIGN KEY (buyer_id) REFERENCES buyers(id)
 );
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
